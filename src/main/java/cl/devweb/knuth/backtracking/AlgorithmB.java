63b516f4966c9b712db097780f00e297f772ebc0
German Gonzalez <german.gonzalez@gmail.com>
2018-06-25 10:45:45 -0400

    backtracking impl. empty files :)

---
+++ src/main/java/cl/devweb/knuth/backtracking/AlgorithmB.java
@@ -0,0 +1,24 @@
+package cl.devweb.knuth.backtracking;
+
+public class AlgorithmB {
+
+    /*****
+     * Algorithm B
+     * From The Art of Computer Programming
+     * Introduction to Backtracking
+     * p.11
+     * 
+     * Volume 4 pre-fascicle 5B
+     * (May 31, 2018)
+     * 
+     * 20.Jun.2018
+     * 
+     */
+	
+	public static void main(String[] args) {
+	
+
+
+	}
+
+}
